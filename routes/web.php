b49537960e8904dcc6dabe502b5ab35704bd8288
Miguel Serrano <miguelangelserranoaraque@gmail.com>
2025-02-25 15:22:13 -0500

    Rutas basico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/<products', function () {
+    echo 'LISTADO DE PRODUCTOS DEL ECOMERCE';
+});
+
+
+Route::get('/products/{name}', function ($name) {
+    echo 'EL PRODUCTO ES:', $name;
+});
+
